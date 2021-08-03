1ad88db22681f1b467f3b549e2ae5a149d464709
nikhil076 <nikhil.arvind7@gmail.com>
2021-08-03 17:39:10 +0530

    Added functionality to calculate wage of employee till working hours is 100 or till 20 days

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -4,6 +4,7 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOURS = 20;
 const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 100;
 
 function getWorkingHours(empCheck)
 {
@@ -18,11 +19,13 @@ function getWorkingHours(empCheck)
 	}
 }
 
-let empHrs = 0;
-for(let day = 0;day<NUM_OF_WORKING_DAYS;day++)
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+while(totalEmpHrs<=MAX_HRS_IN_MONTH  && totalWorkingDays<NUM_OF_WORKING_DAYS)
 {
+	totalWorkingDays++;
 	let empCheck = Math.floor(Math.random() * 10) % 3;
-	empHrs+= getWorkingHours(empCheck);
+	totalEmpHrs+= getWorkingHours(empCheck);
 }
-let empWage = empHrs * WAGE_PER_HOURS;
-console.log("Total Employee Wage is :"+empWage);
+let empWage = totalEmpHrs * WAGE_PER_HOURS;
+console.log("Total Employee Wage is :"+empWage+" Total Hours :"+totalEmpHrs+" total Working days : "+totalWorkingDays);
