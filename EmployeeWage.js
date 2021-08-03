ef292d794bea5f2f1638c601013b8275c41d9df8
nikhil076 <nikhil.arvind7@gmail.com>
2021-08-03 17:01:26 +0530

    Added part time and full time employee details to calculate their salary

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,11 +1,22 @@
-const IS_ABSENT = 0;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOURS = 20;
 
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if(empCheck == IS_ABSENT)
-{
-	console.log("Employee is absent");
+let empHrs = 0;
+let empCheck = Math.floor(Math.random() * 10) % 3;
+
+switch(empCheck){
+	case IS_PART_TIME :
+		empHrs = PART_TIME_HOURS;
+		break;
+	case IS_FULL_TIME :
+		empHrs = FULL_TIME_HOURS;
+		break;
+	default :
+		empHrs = 0;
 }
-else
-{
-	console.log("Employee is present");
-}
+
+let empWage = empHrs * WAGE_PER_HOURS;
+console.log("Employee Wage is :"+empWage);
