5d62f898bb1331028879a43bc7b546a7e819984d
nikhil076 <nikhil.arvind7@gmail.com>
2021-08-05 16:21:01 +0530

    Ability to check the name start with capital using regex and used regex pattern and try catch for error

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -14,7 +14,12 @@ class EmployeePayrollData{
 
     //getter and setter method
     get name(){ return this._name;}
-    set name(name){this._name = name;}
+    set name(name){
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+        if(nameRegex.test(name))
+            this._name = name;
+        else throw "Name is incorrect";
+    }
 
     //method
     toString(){
@@ -26,7 +31,11 @@ class EmployeePayrollData{
 
 let employeePayrollData = new EmployeePayrollData(1,"Mark",30000);
 console.log(employeePayrollData.toString());
-employeePayrollData.name = "John";
-console.log(employeePayrollData.toString());
-let newEmployeePayrollData = new EmployeePayrollData(1,"terrisa",30000,"F",new Date());
+try{
+    employeePayrollData.name = "john";
+    console.log(employeePayrollData.toString());
+} catch(e){
+    console.error(e);
+}
+let newEmployeePayrollData = new EmployeePayrollData(1,"Terrisa",30000,"F",new Date());
 console.log(newEmployeePayrollData.toString());
